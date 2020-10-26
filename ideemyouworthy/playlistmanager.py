477025f586a69ec61c8c21e7634625d9a7234789
TheKingElessar <wasabimath@gmail.com>
2020-10-26 16:11:34 -0600

    Added unused function to completely delete playlist

--- ideemyouworthy/playlistmanager.py
+++ ideemyouworthy/playlistmanager.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from urllib.parse import urlparse
 from posixpath import basename, dirname
 
+import win32com
+
 
 class PlaylistManager:
 
@@ -93,7 +95,12 @@ class PlaylistManager:
             if not custom_playlists_dict[playlist] == "[example--will not be used]":
                 parsed_dict[playlist_object["name"]] = uri_playlist
 
+            custom_playlists_dict[playlist] = playlist_object["name"]
+
         self.logger.log("Read custom playlists")
+
+        self.store_custom_playlists(custom_playlists_dict)
+        self.logger.log("Stored custom playlists")
 
         return parsed_dict
 
@@ -108,3 +115,39 @@ class PlaylistManager:
         del custom_playlists_dict[playlist_url]
         self.store_custom_playlists(custom_playlists_dict)
         self.logger.log("Removed custom playlist: " + playlist_url)
+
+    def delete_stored_playlist(self, playlist):
+        custom_playlists_dict = json.loads(self.custom_playlist_file.read_text())
+        for custom_playlist in custom_playlists_dict:
+            if custom_playlists_dict[custom_playlist] == playlist:
+                self.delete_custom_playlist(custom_playlist)
+
+        self.store_custom_playlists(custom_playlists_dict)
+
+        stored_playlists_dict = json.loads(self.master_playlist_file.read_text())
+        for stored_playlist in stored_playlists_dict:
+            if stored_playlist == playlist:
+                del stored_playlists_dict[stored_playlist]
+
+        self.store_user_playlists(stored_playlists_dict)
+
+        itunes = win32com.client.Dispatch("iTunes.Application")
+
+        itunes_sources = itunes.Sources
+        itunes_playlists = None
+        for source in itunes_sources:
+            if source.Kind == 1:
+                itunes_playlists = source.Playlists
+
+        itunes_playlists_dict = {}
+        playlists_left = itunes_playlists.Count
+        while playlists_left != 0:
+            playlist = itunes_playlists.Item(playlists_left)
+            blacklist = {"Voice Memos", "Genius", "Audiobooks", "Podcasts", "TV Shows", "Movies", "Library", "Music"}
+            if playlist.Name not in blacklist:
+                itunes_playlists_dict[playlist.Name] = playlist
+            playlists_left -= 1
+
+        for itunes_playlist in itunes_playlists_dict:
+            if itunes_playlist == playlist:
+                itunes_playlists_dict[itunes_playlist].Delete()
